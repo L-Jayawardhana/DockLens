f190b35f46785a5c55b8f279f37ddd8545c68250
L-Jayawardhana <lakdinurameshjayawardhana@gmail.com>
2026-03-19 17:52:29 +0530

    create utils.go for connecting necessary utils for the application

---
+++ utils.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func timeAgo(t time.Time) string {
+	d := time.Since(t)
+	switch {
+	case d < time.Minute:
+		return "just now"
+	case d < time.Hour:
+		return fmt.Sprintf("%dm ago", int(d.Minutes()))
+	case d < 24*time.Hour:
+		return fmt.Sprintf("%dh ago", int(d.Hours()))
+	case d < 7*24*time.Hour:
+		return fmt.Sprintf("%dd ago", int(d.Hours()/24))
+	default:
+		return t.Format("Jan 02, 2006")
+	}
+}
